3666065c68ea19e18143befb35bcc53245662485
Raynor Elgie <elgieraynor@gmail.com>
2020-08-07 22:14:10 -0400

    added facts alongside avgs

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 averages = {}
+facts = {}
 
 def parseAvgs():
     with open('avgs.txt','r') as f:
         for line in f:
             lst = line.split()
             averages[lst[0]] = float(lst[1])
-    
+
+def parseFacts():
+    global facts
+    isKey = True
+    with open('facts.txt','r') as f:
+        stripped = [line.strip() for line in f.readlines()]
+        for line in stripped:
+            if isKey:
+                key = line
+                isKey = False
+            else:
+                fact = line
+                isKey = True
+                facts[key] = fact
 
 def percentComparison(key, value):
     global averages
@@ -18,13 +32,17 @@ def percentComparison(key, value):
         return ("Your " + key + " is " + str(percentDiff) + "% less than average")
     else:
         return ("Your " + key + " is average!")
-    
-parseAvgs()
-print(averages)
 
+
+
+parseAvgs()
+parseFacts()
+print(facts)
+print(averages)
 
         
 
 for key in averages.keys():
     print("What is your " + key)
     print(percentComparison(key,float(input(" "))))
+    print(facts[key])
