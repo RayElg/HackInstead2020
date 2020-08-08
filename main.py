6e228fdd174a9d43d0d96b89b43197cdf6b519e4
Raynor Elgie <elgieraynor@gmail.com>
2020-08-07 21:21:29 -0400

    percentComparison method improved

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ def parseAvgs():
     with open('avgs.txt','r') as f:
         for line in f:
             lst = line.split()
-            averages[lst[0]] = lst[1]
+            averages[lst[0]] = float(lst[1])
     
 
 def percentComparison(key, value):
-    incomeAvg = 50000
-    if(key == "income"): #Temporary way of evaluating keys
-        percentDiff = (abs(value-incomeAvg)/((incomeAvg+value)/2)) * 100
-        if (value > incomeAvg):
-            return ("Your " + key + " is " + str(percentDiff) + "% greater than average")
-        elif (incomeAvg > value):
-            return ("Your " + key + " is " + str(percentDiff) + "% less than average")
-        else:
-            return ("Your " + key + " is average!")
-
-print(percentComparison("income",10000))
-        
+    global averages
+    avg = averages[key]
+    
+    percentDiff = (abs(value-avg)/((avg+value)/2)) * 100
+    if (value > avg):
+        return ("Your " + key + " is " + str(percentDiff) + "% greater than average")
+    elif (avg > value):
+        return ("Your " + key + " is " + str(percentDiff) + "% less than average")
+    else:
+        return ("Your " + key + " is average!")
+    
 parseAvgs()
 print(averages)
+
+print(percentComparison("wage",10000))
+        
+
