b3902194f3801e31658992f43e2b6c21ca57b737
Gar <aymanasgar@gmail.com>
2020-08-07 21:42:02 -0400

    Create NotNumericalMethods.py

---
+++ NotNumericalMethods.py
@@ -0,0 +1,17 @@
+#read from a non-numerical txt file
+
+
+
+#sort the value of non numerical values into respective arrays 
+
+#get user input as key 
+
+#if user input key is equal to map key 
+
+#print % of population with trait
+
+
+
+#Note create method to sort through txt file to create respective maps for each of the traits, eyes, country, 
+
+
