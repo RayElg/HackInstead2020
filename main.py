1e8dca54719779bf3dfe616e41b578111471eb6a
Raynor Elgie <elgieraynor@gmail.com>
2020-08-07 20:51:43 -0400

    Added mockup of comparison

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-def percentComparison():
+def percentComparison(key, value):
+    incomeAvg = 50000
+    if(key == "income"): #Temporary way of evaluating keys
+        percentDiff = (abs(value-incomeAvg)/((incomeAvg+value)/2)) * 100
+        if (value > incomeAvg):
+            return ("Your " + key + " is " + str(percentDiff) + "% greater than average")
+        elif (incomeAvg > value):
+            return ("Your " + key + " is " + str(percentDiff) + "% less than average")
+        else:
+            return ("Your " + key + " is average!")
+
+print(percentComparison("income",10000))
+        
