227774c43145ee992a25943a9e71ae339e26d942
Gar <aymanasgar@gmail.com>
2020-08-08 10:08:05 -0400

    Update NotNumericalMethods.py

--- NotNumericalMethods.py
+++ NotNumericalMethods.py
@@ -13,18 +13,26 @@ def ReturnComparison(UserKey):
     global EyeColours
     return ("You have a " +UserKey+ " which is a trait shared by  "+EyeColours.get(UserKey))
 
+#list all keys within an dictionary
+def ListKeysOfDic(Dictionary):
+    for keys in Dictionary:
+        print(" " +key+ " ")
+
 
 #prompt user for input
 def PromptUser():
-    for keys in EyeColours: s
-        print(" " +key+ " ")
+    ListKeys(EyeColours)
+
+def GetUserInput():
     UserEyeColour = input("From the list of Eye colours above enter which colour most accuratly describes you: ")
     return(UserEyeColour)
-    
+
 
 FillEyeColours()
 print(EyeColours)
-ReturnComparison(PromptUser)
+val = GetUserInput()
+print(val)
+##ReturnComparison(PromptUser)
 
 
 
